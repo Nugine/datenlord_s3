259b3847e2717d0fca94df11df7acc954d1afb9d
Nugine <nugine@foxmail.com>
2020-09-22 11:46:46 +0800

    fix

--- scripts/mint.sh
+++ scripts/mint.sh
@@ -1,5 +1,5 @@
 docker run \
-    -e "SERVER_ENDPOINT=localhost:8014" -e "DOMAIN=localhost"  \
+    -e "SERVER_ENDPOINT=127.0.0.1:8014" -e "DOMAIN=localhost"  \
     -e "ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" -e "SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" \
     -e "ENABLE_HTTPS=0" -e "ENABLE_VIRTUAL_STYLE=1" \
     minio/mint
